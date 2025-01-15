82ce6a5146f2e89da576e3fb7cbb7c5260537f51
Jason Swift <anemele@outlook.com>
2025-01-15 13:20:48 +0800

    upgrade to 0.1.9; update `statpage`: more readable output;

--- src/py_pdf/statpage.py
+++ src/py_pdf/statpage.py
@@ -3,25 +3,28 @@
 """
 
 import argparse
+from dataclasses import dataclass
 from pathlib import Path
-from typing import Iterable
+from typing import Iterable, Optional
 
 from pypdf import PdfReader
 
 
-def get_page_num(path: Path) -> tuple[int, int]:
+@dataclass
+class PathAndCount:
+    path: Path
+    count: int
+
+
+def read_pdf(path: Path) -> Optional[PathAndCount]:
     try:
         reader = PdfReader(path)
-        return 1, len(reader.pages)
+        return PathAndCount(path, len(reader.pages))
     except Exception as e:
         print(f"Error reading {path}: {e}")
-        return 0, 0
 
 
-def stat_pdf(paths: Iterable[Path]) -> tuple[int, int]:
-    file_count = 0
-    page_count = 0
-
+def stat_pdf(paths: Iterable[Path]) -> Iterable[PathAndCount]:
     def chain_files():
         for path in paths:
             if path.is_file():
@@ -29,12 +32,29 @@ def stat_pdf(paths: Iterable[Path]) -> tuple[int, int]:
             else:
                 yield from path.glob("**/*.pdf")
 
-    for file in chain_files():
-        res = get_page_num(file)
-        file_count += res[0]
-        page_count += res[1]
+    res = filter(None, map(read_pdf, chain_files()))
+    return res
 
-    return file_count, page_count
+
+def print_result(result: Iterable[PathAndCount]):
+    file_count = 0
+    page_count = 0
+    for item in result:
+        file_count += 1
+        page_count += item.count
+    print(f"Total files: {file_count}")
+    print(f"Total pages: {page_count}")
+
+
+def print_result_verbose(result: Iterable[PathAndCount]):
+    file_count = 0
+    page_count = 0
+    for item in result:
+        file_count += 1
+        page_count += item.count
+        print(f"{item.count:4d}    {item.path}")
+    print(f"Total files: {file_count}")
+    print(f"Total pages: {page_count}")
 
 
 def main():
@@ -43,13 +63,22 @@ def main():
         description=__doc__,
         formatter_class=argparse.RawTextHelpFormatter,
     )
-    parser.add_argument("path", nargs="+", type=Path, help="Paths to PDF files")
+    parser.add_argument("path", nargs="*", type=Path, help="Paths to PDF files")
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Show verbose output"
+    )
     args = parser.parse_args()
     paths: list[Path] = args.path
+    verbose: bool = args.verbose
 
-    file_count, page_count = stat_pdf(paths)
-    print(f"Total files: {file_count}")
-    print(f"Total pages: {page_count}")
+    if len(paths) == 0:
+        paths = [Path.cwd()]
+
+    res = stat_pdf(paths)
+    if verbose:
+        print_result_verbose(res)
+    else:
+        print_result(res)
 
 
 if __name__ == "__main__":
--- tests/test_statpage.py
+++ tests/test_statpage.py
@@ -1,12 +1,15 @@
 from pathlib import Path
 
-from py_pdf.statpage import stat_pdf
+from py_pdf.statpage import PathAndCount, stat_pdf
 
 A4 = Path("tests/sample/A4.pdf")
 A3 = Path("tests/sample/A3-booklet.pdf")
 
 
 def test_it():
-    assert stat_pdf([A4]) == (1, 10)
-    assert stat_pdf([A3]) == (1, 4)
-    assert stat_pdf([A4, A3]) == (2, 14)
+    def f(paths):
+        return list(stat_pdf(paths))
+
+    assert f([A4]) == [PathAndCount(A4, 10)]
+    assert f([A3]) == [PathAndCount(A3, 4)]
+    assert f([A4, A3]) == [PathAndCount(A4, 10), PathAndCount(A3, 4)]
